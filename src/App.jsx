b0e3f086910e477c3d90ca94119dc1fa2b67a337
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-08-10 15:47:44 +0900

    feat/모바일 뷰 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import "./App.css";
-import { Outlet } from "react-router-dom";
+import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <div className='phone-frame'>
       <Outlet />
-    </>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
