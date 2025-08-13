4528801c8ca3f11142f1a51110c54b81f0832328
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-14 00:41:27 +0900

    style/#8/스플래시 스타일 컴포넌트 파일 분리

--- src/components/onboarding/Splash.jsx
+++ src/components/onboarding/Splash.jsx
@@ -1,27 +1,12 @@
 import React from 'react'
-import styled from 'styled-components'
 import splachImg from '@/assets/images/splash-logo.svg'
-
-const SplashContainer = styled.div`
-  display: flex;
-  position: fixed;
-  justify-content: center;
-  align-items: center;
-  background-color: #25af94;
-  width: 24.5625rem;
-  height: 53.25rem;
-`
-const Logo = styled.img`
-  width: 13.875rem;
-  height: 6.48938rem;
-  flex-shrink: 0;
-`
+import * as S from './Splash.Styled.js'
 
 function Splash() {
   return (
-    <SplashContainer>
-      <Logo src={splachImg} alt='Splash Logo' />
-    </SplashContainer>
+    <S.SplashContainer>
+      <S.Logo src={splachImg} alt='Splash Logo' />
+    </S.SplashContainer>
   )
 }
 
