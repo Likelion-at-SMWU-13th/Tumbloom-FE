da9a2def6c12d920ecaa7dcfcbcfaff91e6d3ff8
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-13 12:59:44 +0900

    style/#4/Header 스타일 컴포넌트 파일 분리

---
+++ src/components/common/Header.Styled.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+
+export const HeaderBox = styled.div`
+  display: grid;
+  grid-template-columns: 3.5rem 1fr 3.5rem;
+  width: 24.5625rem;
+  height: 3.5rem;
+  box-sizing: border-box;
+  position: sticky;
+  top: 0;
+  z-index: 1000;
+  padding-top: 0;
+  flex-shrink: 0;
+`
+
+export const LeftArea = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 0.5rem;
+  padding-left: 1.65rem;
+`
+
+export const BackBtn = styled.img`
+  width: 1.5rem;
+  height: 1.5rem;
+  flex-shrink: 0;
+  aspect-ratio: 1/1;
+  cursor: pointer;
+`
+
+export const TitleArea = styled.div`
+  margin: 0;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  color: #000;
+  text-align: center;
+  font-family: 'Pretendard Variable';
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  padding-top: 0.5rem;
+`
+
+export const RightArea = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  padding-top: 0.5rem;
+`
