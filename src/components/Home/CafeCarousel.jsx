2ebbed73a54255b36b018dfae490930f69414a4a
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-08-15 00:27:37 +0900

    chore/#3/AI 추천 카페 객체 배열 임시 생성

--- src/components/Home/CafeCarousel.jsx
+++ src/components/Home/CafeCarousel.jsx
@@ -42,6 +42,40 @@ const CafeCarousel = () => {
       bookmark: false,
     },
   ]
+
+  const CafeListAI = [
+    {
+      image: cafeImg,
+      cafeName: '카페1',
+      address: '서울 용산구 남영동1가',
+      bookmark: true,
+    },
+    {
+      image: 'https://i.pinimg.com/736x/88/e6/52/88e652d26358fdff316db07ac08d13f3.jpg',
+      cafeName: '카페2',
+      address: '서울 용산구 남영동2가',
+      bookmark: false,
+    },
+    {
+      image: 'https://i.pinimg.com/1200x/60/0c/47/600c47609169e67f27934307a2c0c994.jpg',
+      cafeName: '카페3',
+      address: '서울 용산구 남영동3가',
+      bookmark: false,
+    },
+    {
+      image: 'https://i.pinimg.com/736x/70/30/07/7030076830ff36ba81861cc77d3f1503.jpg',
+      cafeName: '카페4',
+      address: '서울 용산구 남영동4가',
+      bookmark: false,
+    },
+    {
+      image: 'https://i.pinimg.com/736x/dc/60/45/dc6045915b8af252220f17127f8d6666.jpg',
+      cafeName: '카페5',
+      address: '서울 용산구 남영동5가',
+      bookmark: false,
+    },
+  ]
+
   const [active, setActive] = useState(0)
 
   const left = mod(active - 1, CafeList.length)
