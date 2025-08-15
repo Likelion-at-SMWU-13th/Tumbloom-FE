a3d4c706d669e32ba00422b582e4d0d14c7a95ea
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-16 02:35:54 +0900

    style/#21/스탬프 적립하기 페이지 스타일 컴포넌트 적용

--- src/components/getStamp/StampAccess.jsx
+++ src/components/getStamp/StampAccess.jsx
@@ -1,20 +1,135 @@
 import React from 'react'
 import CafeImg from '@/assets/images/get-stamp-cafe.png'
 import Header from '@/components/common/Header'
+import styled from 'styled-components'
 
-function StampAccess() {
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const BackgroundImg = styled.img``
+
+const Title = styled.span`
+  padding: 11.56rem 0 0.94rem 0;
+  color: #000;
+  font-family: 'Pretendard Variable';
+  font-size: 1.75rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 130%;
+  text-align: center;
+  border: 1px solid pink;
+`
+const CafeName = styled.span`
+  color: #25af94;
+  text-align: center;
+  font-family: 'Pretendard Variable';
+  font-size: 1.75rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 130%; /* 2.275rem */
+`
+const Desc = styled.span`
+  color: #767676;
+  text-align: center;
+  font-family: 'Pretendard Variable';
+  font-size: 0.9375rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 130%;
+  border: 1px solid yellow;
+`
+const AccessCodeBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const AccessCodeHeader = styled.span`
+  color: #000;
+  text-align: center;
+  font-family: 'Pretendard Variable';
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  border: 1px solid blue;
+  padding: 1.81rem 0 0.62rem 0;
+`
+const InputField = styled.div`
+  display: flex;
+  justify-content: center;
+  border: 1px solid black;
+  padding-left: 3.3rem;
+`
+
+const CodeInput = styled.input`
+  width: 16.4375rem;
+  height: 3.0625rem;
+  flex-shrink: 0;
+  border-radius: 0.3125rem;
+  background: #e9f4f2;
+  border: none;
+  padding-left: 1.12rem;
+  color: #005c4a;
+  font-family: Inter;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  outline: none;
+
+  &::placeholder {
+    color: #b1cac5;
+    font-family: 'Pretendard Variable';
+    font-size: 1.25rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    border: 1px solid black;
+  }
+`
+const CompleteBtn = styled.button`
+  width: 3.125rem;
+  height: 2.8125rem;
+  flex-shrink: 0;
+  border-radius: 0.3125rem;
+  opacity: 0.6;
+  background: #fff;
+  color: #767676;
+  font-family: 'Pretendard Variable';
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  right: 3.3rem;
+  position: relative;
+  transform: translateY(0.17rem);
+  border: none;
+  cursor: pointer;
+`
+
+function StampAccess({ cafeName = '그린카페' }) {
   return (
-    <div>
+    <>
       <Header title='스탬프 적립하기' />
-      <h1>그린카페에서 텀블러 테이크아웃할게요</h1>
-      <p>음료 받을 때 직원에게 화면을 보여주고 확인코드를 받으면 스탬프를 적립해요</p>
-      <div>
-        <h3>직원확인코드</h3>
-        <input placeholder='확인코드 입력'>
-          <button>완료</button>
-        </input>
-      </div>
-    </div>
+      <Container>
+        <BackgroundImg src={CafeImg} />
+        <Title>
+          <CafeName>{cafeName}</CafeName> 에서 <br /> 텀블러 테이크아웃할게요
+        </Title>
+        <Desc>
+          음료 받을 때 직원에게 화면을 보여주고 <br />
+          확인코드를 받으면 스탬프를 적립해요
+        </Desc>
+        <AccessCodeBox>
+          <AccessCodeHeader>직원확인코드</AccessCodeHeader>
+          <InputField>
+            <CodeInput placeholder='확인코드 입력' />
+            <CompleteBtn>완료</CompleteBtn>
+          </InputField>
+        </AccessCodeBox>
+      </Container>
+    </>
   )
 }
 
