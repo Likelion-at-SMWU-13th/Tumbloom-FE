d83b07c47bd1aa4cd48c547c42c02a733a74df60
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-15 16:15:07 +0900

    style/#10/회원가입 입력창 스타일 지정 및 props 적용

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Input(props) {
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const Label = styled.label`
+  align-self: stretch;
+  color: #000;
+  font-family: 'Pretendard Variable';
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+const InputBox = styled.input`
+  width: 20.875rem;
+  height: 3.1875rem;
+  flex-shrink: 0;
+  border-radius: 0.625rem;
+  background: #efefef;
+`
+
+function Input(label, placeholder) {
   return (
-    <div>
-      <span>닉네임</span>
-      <input type='text' placeholder='닉네임 입력 (2~8자)'></input>
-    </div>
+    <InputContainer>
+      <Label>{label}</Label>
+      <InputBox type='text' placeholder={placeholder}></InputBox>
+    </InputContainer>
   )
 }
 
