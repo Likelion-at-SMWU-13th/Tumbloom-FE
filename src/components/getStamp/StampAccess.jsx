725d87ce9e3d426b0a9ae2f8d750363e721ddaea
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-16 01:38:58 +0900

    feat/#21/스탬프 적립하기 페이지 UI 기본 구조

--- src/components/getStamp/StampAccess.jsx
+++ src/components/getStamp/StampAccess.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import CafeImg from '@/assets/images/get-stamp-cafe.png'
+import Header from '@/components/common/Header'
 
-function StampAccess(props) {
-  return <div></div>
+function StampAccess() {
+  return (
+    <div>
+      <Header title='스탬프 적립하기' />
+      <h1>그린카페에서 텀블러 테이크아웃할게요</h1>
+      <p>음료 받을 때 직원에게 화면을 보여주고 확인코드를 받으면 스탬프를 적립해요</p>
+      <div>
+        <h3>직원확인코드</h3>
+        <input placeholder='확인코드 입력'>
+          <button>완료</button>
+        </input>
+      </div>
+    </div>
+  )
 }
 
 export default StampAccess
