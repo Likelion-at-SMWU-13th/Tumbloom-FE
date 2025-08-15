2d441aa368565b953d070b0c09a73e8ed567c98b
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-15 18:02:29 +0900

    style/#10/등록 버튼 스타일 컴포넌트 파일 분리

--- src/components/Button/RegisterBtn.jsx
+++ src/components/Button/RegisterBtn.jsx
@@ -1,50 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const BtnContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 2.5rem 0 0 0;
-`
-
-const Button = styled.button`
-  display: flex;
-  width: 20.75rem;
-  height: 3.5rem;
-  padding: 0.625rem;
-  justify-content: center;
-  align-items: center;
-  gap: 0.625rem;
-  flex-shrink: 0;
-  border-radius: 1.875rem;
-  text-align: center;
-  font-family: 'Pretendard Variable';
-  font-size: 1.125rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  background: #25af94;
-  color: #fff;
-  border: 1.4px solid #828282;
-  border: none;
-  cursor: pointer;
-
-  &:disabled {
-    color: #25af94;
-    border: 1.4px solid #25af94;
-    background: #fff;
-    pointer-events: none;
-  }
-`
+import * as S from './RegisterBtn.Styled.js'
 
 function RegisterBtn({ btnName, onClick, disabled }) {
   return (
-    <BtnContainer>
-      <Button type='button' onClick={onClick} disabled={disabled}>
+    <S.BtnContainer>
+      <S.Button type='button' onClick={onClick} disabled={disabled}>
         {btnName}
-      </Button>
-    </BtnContainer>
+      </S.Button>
+    </S.BtnContainer>
   )
 }
 
