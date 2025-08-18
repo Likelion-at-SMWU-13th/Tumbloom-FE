721c3ca5d0959c18a00a3a4f4c0cf8e77f7c21be
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-19 01:47:19 +0900

    chore/#31/카페 기본정보 박스 컴포넌트 분리

--- src/components/CafeInfo/CafeDetail.jsx
+++ src/components/CafeInfo/CafeDetail.jsx
@@ -4,9 +4,7 @@ import styled from 'styled-components'
 import scrabOn from '@/assets/icons/clicked-bookmark.svg'
 import scrabOff from '@/assets/icons/bookmark.svg'
 import cafeDetail from '@/assets/images/cafe-detail.svg'
-import locIcon from '@/assets/icons/cafe-info-loc.svg'
-import timeIcon from '@/assets/icons/cafe-info-time.svg'
-import numIcon from '@/assets/icons/cafe-info-num.svg'
+import CafeInfoBox from './CafeInfoBox'
 
 const CafeHeaderWrapper = styled.div`
   display: flex;
@@ -93,53 +91,6 @@ const MenuTap = styled.span`
   padding: 1.25rem 0 0.69rem 0;
 `
 
-const InfoBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-left: 1.25rem;
-`
-
-const InfoName = styled.span`
-  border: 1px solid black;
-  color: #ababab;
-  font-family: 'Pretendard Variable';
-  font-size: 0.9375rem;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-  display: flex;
-  gap: 0.25rem;
-`
-
-const Location = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-top: 1.25rem;
-  padding-bottom: 1.56rem;
-`
-
-const Time = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-bottom: 1.56rem;
-`
-
-const Num = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-bottom: 1.19rem;
-`
-
-const InfoDetail = styled.span`
-  color: #000;
-  font-family: 'Pretendard Variable';
-  font-size: 0.9375rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  padding-top: 0.31rem;
-`
-
 const StampBtn = styled.button`
   color: #fff;
   font-family: 'Pretendard Variable';
@@ -181,39 +132,7 @@ function CafeDetail() {
         <InfoTap>기본정보</InfoTap>
         <MenuTap>메뉴</MenuTap>
       </FilterTap>
-      <InfoBox>
-        <Location>
-          <InfoName>
-            <img src={locIcon} />
-            위치
-          </InfoName>
-          <InfoDetail>
-            월 09:00 - 15:00
-            <br /> 화 09:00 - 15:00 <br />수 09:00 - 15:00 <br />목 09:00 - 15:00 <br />금 09:00 -
-            15:00
-            <br /> 토 09:00 - 15:00
-            <br />일 09:00 - 15:00
-          </InfoDetail>
-        </Location>
-        <Time>
-          <InfoName>
-            <img src={timeIcon} />
-            영업시간
-          </InfoName>
-          <InfoDetail>
-            서울시 용산구 청파로47 78 제1호 내제1층호
-            <br />
-            4호선 숙대입구역 10번 출구에서 572m
-          </InfoDetail>
-        </Time>
-        <Num>
-          <InfoName>
-            <img src={numIcon} />
-            연락처
-          </InfoName>
-          <InfoDetail>02-222-222</InfoDetail>
-        </Num>
-      </InfoBox>
+      <CafeInfoBox />
       <BtnContainer>
         <StampBtn>스탬프 적립하기</StampBtn>
       </BtnContainer>
