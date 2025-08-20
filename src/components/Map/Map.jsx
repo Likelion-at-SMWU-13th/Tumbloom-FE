d750668cb8e771200a96b4c7e4b51f454e5a0f0c
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-21 01:21:48 +0900

    feat/#46/버튼 활성화 / 비활성화 기능

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
-import SearchBox from '@/components/Coupon/CafeInputField'
+import SearchBox from '@/components/Map/SearchBox'
 import MapBtn from './MapBtn'
-import sparkleIcon from '@/assets/icons/sparkle.svg'
-import myCouponIcon from '@/assets/icons/my-coupon.svg'
-import hotIcon from '@/assets/icons/hot-place.svg'
+import Footer from '../common/Footer'
+import sparkOff from '@/assets/icons/sparkleOff.svg'
+import sparkOn from '@/assets/icons/sparkleOn.svg'
+import couponOn from '@/assets/icons/couponOn.svg'
+import couponOff from '@/assets/icons/couponOff.svg'
+import hotOff from '@/assets/icons/hotOff.svg'
+import hotOn from '@/assets/icons/hotOn.svg'
 import mapIcon from '@/assets/icons/my-loc.svg'
 import listIcon from '@/assets/icons/list.svg'
 import styled from 'styled-components'
+import Sprout from '@/components/Map/CafeSprout'
+import { useState } from 'react'
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  padding-top: 3rem;
 `
 
 const HeaderBtns = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  gap: 0.8rem;
-  border: 1px solid black;
+  gap: 0.6rem;
   padding-top: 0.88rem;
 `
 
 const FooterBtns = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
-  border: 1px solid black;
-  bottom: 0;
+  justify-content: center;
+  gap: 13rem;
+  transform: translateY(40rem);
 `
 
 const MyLocBtn = styled.button`
@@ -40,21 +46,71 @@ const MyLocBtn = styled.button`
   border: none;
 `
 
+const ListBtn = styled.button`
+  display: inline-flex;
+  height: 2.375rem;
+  padding: 0 0.9375rem;
+  align-items: center;
+  gap: 0.4375rem;
+  flex-shrink: 0;
+  border-radius: 18.75rem;
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.15);
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+`
+
+const ListBtnIcon = styled.img`
+  aspect-ratio: 1/1;
+`
+
+const ListBtnName = styled.span`
+  color: #000;
+  text-align: center;
+  font-family: 'Pretendard Variable';
+  font-size: 0.9375rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
+
 function Map() {
+  const [active, setActive] = useState('')
+  const select = (key) => setActive(key)
+
   return (
     <Wrapper>
       <SearchBox />
       <HeaderBtns>
-        <MapBtn img={sparkleIcon} name='AI 내취향 추천' />
-        <MapBtn img={myCouponIcon} name='쿠폰 보유' />
-        <MapBtn img={hotIcon} name='인기' />
+        <MapBtn
+          img={active === 'ai' ? sparkOn : sparkOff}
+          name='AI 내취향 추천'
+          onClick={() => select('ai')}
+          isActive={active === 'ai'}
+        />
+        <MapBtn
+          img={active === 'coupon' ? couponOn : couponOff}
+          name='쿠폰 보유'
+          onClick={() => select('coupon')}
+          isActive={active === 'coupon'}
+        />
+        <MapBtn
+          img={active === 'hot' ? hotOn : hotOff}
+          name='인기'
+          onClick={() => select('hot')}
+          isActive={active === 'hot'}
+        />
       </HeaderBtns>
       <FooterBtns>
         <MyLocBtn>
           <img src={mapIcon} />
         </MyLocBtn>
-        <MapBtn img={listIcon} name='목록보기' />
+        <ListBtn>
+          <ListBtnIcon src={listIcon} />
+          <ListBtnName>목록보기</ListBtnName>
+        </ListBtn>
       </FooterBtns>
+      <Footer />
     </Wrapper>
   )
 }
