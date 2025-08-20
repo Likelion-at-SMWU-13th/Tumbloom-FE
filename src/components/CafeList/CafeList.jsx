686fa2d4ae2d7eb34f6cbbdc9d1e527e8a528115
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-20 17:11:26 +0900

    style/#42/카페 리스트 스타일 컴포넌트 파일 분리

--- src/components/CafeList/CafeList.jsx
+++ src/components/CafeList/CafeList.jsx
@@ -2,41 +2,7 @@ import React from 'react'
 import Header from '../common/Header'
 import CafeListCard from '../common/CafeListCard'
 import cafeListLine from '@/assets/images/cafe-list-line.svg'
-import styled from 'styled-components'
-
-const ScrollArea = styled.div`
-  display: flex;
-  width: 24.5625rem;
-  flex-direction: column;
-`
-
-const Wrapper = styled.div`
-  width: 24.5625rem;
-  flex-shrink: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border-top: 1px solid #d9d9d9;
-  flex: 1;
-  min-height: 0;
-  overflow-y: auto;
-  & > *:not(:first-child) {
-    border-top: 1px solid #d9d9d9;
-  }
-`
-
-const CafeListImg = styled.img`
-  width: 22.1875rem;
-  height: 0.0625rem;
-`
-
-// const Pad = styled.div`
-//   display: flex;
-//   border: 1px solid blue;
-//   width: 100%;
-//   height: 1rem;
-// `
+import * as S from '@/components/CafeList/CafeList.Styled'
 
 function CafeList() {
   const CafeList = [
@@ -52,14 +18,14 @@ function CafeList() {
   return (
     <>
       <Header title='카페 목록보기' />
-      <ScrollArea>
+      <S.ScrollArea>
         {' '}
-        <Wrapper>
+        <S.Wrapper>
           {CafeList.map((item, i) => (
             <CafeListCard key={i} name={item.cafeName} loc={item.location} time={item.time} />
           ))}
-        </Wrapper>
-      </ScrollArea>
+        </S.Wrapper>
+      </S.ScrollArea>
     </>
   )
 }
