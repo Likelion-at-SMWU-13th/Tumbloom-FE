3f4cb5e9f45f579b9b200edbc33c53cd012b9eb4
chootkuri <doyadoya@sookmyung.ac.kr>
2025-08-22 02:12:09 +0900

    feat/#61/상단바에 경로 이동 함수 받도록 설정

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import backBtn from '../../assets/icons/back-btn.svg'
 import * as S from './Header.Styled.js'
-function Header({ title }) {
+
+function Header({ title, onLeftClick }) {
   return (
     <S.HeaderBox>
       <S.LeftArea>
-        <S.BackBtn src={backBtn}></S.BackBtn>
+        <S.BackBtn src={backBtn} onClick={onLeftClick}></S.BackBtn>
       </S.LeftArea>
       <S.TitleArea>{title}</S.TitleArea>
       <S.RightArea></S.RightArea>
