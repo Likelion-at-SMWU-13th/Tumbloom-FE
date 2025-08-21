394576756477016a7db02177cf46766ec44a4d3f
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-08-21 15:48:53 +0900

    feat/#57/쿠폰 교환하기 페이지 연동 완료

--- src/components/Coupon/CouponChange.jsx
+++ src/components/Coupon/CouponChange.jsx
@@ -1,39 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import * as S from './styled'
 import { InputBox } from '../common/Input.Styled'
 import CafeInputField from './CafeInputField'
 import CafeCoupon from './CafeCoupon'
 
-const CouponChange = ({ stamp }) => {
-  const CafeCouponList = [
-    { CafeName: '너드커피', DiscountPrice: '1000', count: 10 },
-    { CafeName: '을의커피', DiscountPrice: '1500', count: 10 },
-    { CafeName: '청파로움', DiscountPrice: '500', count: 20 },
-    { CafeName: '청파맨션', DiscountPrice: '1000', count: 18 },
-    { CafeName: '킷테', DiscountPrice: '500', count: 5 },
-    { CafeName: '와플하우스', DiscountPrice: '1000', count: 20 },
-    { CafeName: '마돈나커피', DiscountPrice: '1500', count: 3 },
-    { CafeName: '쥬케로', DiscountPrice: '1000', count: 20 },
-    { CafeName: '카페나리나무', DiscountPrice: '1300', count: 5 },
-    { CafeName: '레드우드', DiscountPrice: '1000', count: 20 },
-  ]
+const CouponChange = () => {
+  const [couponList, setCouponList] = useState([])
+  const [currentCount, setCurrentCount] = useState(0)
+  const [availableCoupon, setAvailableCoupon] = useState(0)
 
-  const collectStamp = Number(stamp)
-  const avaliableCoupon = Math.floor(collectStamp / 8)
-  const moreCoupon = 8 - collectStamp
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken')
+
+    axios
+      .get('https://tumbloom.store/api/coupons', {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        console.log(res.data)
+        setCouponList(res.data.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken')
+
+    axios
+      .get('https://tumbloom.store/api/users/me/home', {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        console.log(res.data)
+        setCurrentCount(res.data.data.stampStatus.currentCount)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken')
+
+    axios
+      .get('https://tumbloom.store/api/users/me/mypage', {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        console.log(res.data)
+        setAvailableCoupon(res.data.data.availableCoupons)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
+  const moreCoupon = 8 - currentCount
 
   return (
     <S.Wrapper>
       <S.SearchArea>
         <S.ChangeTextBox>
-          {collectStamp >= 8 ? (
+          {currentCount >= 8 ? (
             <S.CurrentChangeText>
-              지금까지 스탬프를 <S.HighlightText>{collectStamp}개</S.HighlightText> 모았어요 <br />{' '}
-              이제 쿠폰 <S.HighlightText>{avaliableCoupon}장</S.HighlightText>으로 교환할 수 있어요
+              지금까지 스탬프를 <S.HighlightText>{currentCount}개</S.HighlightText> 모았어요 <br />{' '}
+              이제 쿠폰 <S.HighlightText>{availableCoupon}장</S.HighlightText>으로 교환할 수 있어요
             </S.CurrentChangeText>
           ) : (
             <S.CurrentChangeText>
-              지금까지 스탬프를 <S.HighlightText>{collectStamp}개</S.HighlightText> 모았어요 <br />{' '}
+              지금까지 스탬프를 <S.HighlightText>{currentCount}개</S.HighlightText> 모았어요 <br />{' '}
               쿠폰 교환까지 <S.HighlightText>{moreCoupon}개</S.HighlightText> 남았어요
             </S.CurrentChangeText>
           )}
@@ -44,18 +82,18 @@ const CouponChange = ({ stamp }) => {
         지금 근처 <S.HighlightText>청파동</S.HighlightText> 카페 쿠폰
       </S.NearCouponText>
       <S.CouponList>
-        {CafeCouponList.filter(({ count }) => Number(count) > 0).map(
-          ({ CafeName, DiscountPrice, count }) => (
+        {couponList
+          .filter(({ remainingQuantity }) => Number(remainingQuantity) > 0)
+          .map(({ cafeId, cafeName, remainingQuantity }) => (
             <CafeCoupon
-              key={CafeName}
-              cafeName={CafeName}
-              price={DiscountPrice}
-              count={count}
+              key={cafeId}
+              cafeName={cafeName}
+              price={'1000'}
+              count={remainingQuantity}
               type={'exchange'}
-              active={collectStamp >= 8}
+              active={currentCount >= 8}
             />
-          ),
-        )}
+          ))}
       </S.CouponList>
     </S.Wrapper>
   )
--- src/components/Coupon/MyCoupon.jsx
+++ src/components/Coupon/MyCoupon.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import CafeCoupon from './CafeCoupon'
 import * as S from './styled'
 import NoData from '../common/NoData'
