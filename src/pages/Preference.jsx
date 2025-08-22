02a0285977d498ac177c33fac4d9b933d2ab8e35
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-08-22 10:43:38 +0900

    chore/#65/뒤로가기 버튼 함수 props로 넘겨주기 완료

--- src/pages/Preference.jsx
+++ src/pages/Preference.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Header from '@/components/common/Header'
 import ChoosePreference from '@/components/Preference/ChoosePreference'
 
 const Preference = () => {
+  const navigate = useNavigate()
+
   return (
     <div>
-      <Header title={'내 카페 취향'} />
+      <Header title={'내 카페 취향'} onLeftClick={() => navigate(-1)} />
       <ChoosePreference />
     </div>
   )
