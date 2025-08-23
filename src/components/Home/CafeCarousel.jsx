7322a6ef0c4584f9bf5d5e7bb3b65cb8472cf05d
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-08-23 15:46:21 +0900

    chore/#88/메인페이지 추천 카페 리스트 목데이터 삭제

--- src/components/Home/CafeCarousel.jsx
+++ src/components/Home/CafeCarousel.jsx
@@ -47,72 +47,6 @@ const CafeCarousel = () => {
       })
   }, [])
 
-  const CafeList = [
-    {
-      image: cafeImg,
-      cafeName: '모모카페',
-      address: '서울 용산구 청파동2가',
-      bookmark: true,
-    },
-    {
-      image: 'https://i.pinimg.com/736x/88/e6/52/88e652d26358fdff316db07ac08d13f3.jpg',
-      cafeName: '청파맨션',
-      address: '서울 용산구 청파동1가',
-      bookmark: false,
-    },
-    {
-      image: 'https://i.pinimg.com/1200x/60/0c/47/600c47609169e67f27934307a2c0c994.jpg',
-      cafeName: '을의커피',
-      address: '서울 용산구 청파동4가',
-      bookmark: false,
-    },
-    {
-      image: 'https://i.pinimg.com/736x/70/30/07/7030076830ff36ba81861cc77d3f1503.jpg',
-      cafeName: '청파로움',
-      address: '서울 용산구 청파동5가',
-      bookmark: false,
-    },
-    {
-      image: 'https://i.pinimg.com/736x/dc/60/45/dc6045915b8af252220f17127f8d6666.jpg',
-      cafeName: '쥬케로',
-      address: '서울 용산구 청파동3가',
-      bookmark: false,
-    },
-  ]
-
-  const CafeListAI = [
-    {
-      image: cafeImg,
-      cafeName: '카페1',
-      address: '서울 용산구 남영동1가',
-      bookmark: true,
-    },
-    {
-      image: 'https://i.pinimg.com/736x/88/e6/52/88e652d26358fdff316db07ac08d13f3.jpg',
-      cafeName: '카페2',
-      address: '서울 용산구 남영동2가',
-      bookmark: false,
-    },
-    {
-      image: 'https://i.pinimg.com/1200x/60/0c/47/600c47609169e67f27934307a2c0c994.jpg',
-      cafeName: '카페3',
-      address: '서울 용산구 남영동3가',
-      bookmark: false,
-    },
-    {
-      image: 'https://i.pinimg.com/736x/70/30/07/7030076830ff36ba81861cc77d3f1503.jpg',
-      cafeName: '카페4',
-      address: '서울 용산구 남영동4가',
-      bookmark: false,
-    },
-    {
-      image: 'https://i.pinimg.com/736x/dc/60/45/dc6045915b8af252220f17127f8d6666.jpg',
-      cafeName: '카페5',
-      address: '서울 용산구 남영동5가',
-      bookmark: false,
-    },
-  ]
-
   const [active, setActive] = useState(0)
   const [tab, setTab] = useState(0)
 
@@ -123,10 +57,6 @@ const CafeCarousel = () => {
   useEffect(() => {
     setActive(0)
   }, [tab])
-
-  // const left = mod(active - 1, currentList.length)
-  // const right = mod(active + 1, currentList.length)
-  // const visible = [left, active, right]
 
   const len = currentList.length
   const left = len ? mod(active - 1, len) : 0
