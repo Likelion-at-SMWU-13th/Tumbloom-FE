1c84ba19c44c9415b73f0f2652ac371d7c13a710
chootkuri <doyadoya@sookmyung.ac.kr>
2025-09-25 23:04:18 +0900

    큐알스캔 페이지 스타일 컴포넌트 파일 분리

--- src/pages/QrScan.jsx
+++ src/pages/QrScan.jsx
@@ -3,64 +3,7 @@ import QrScanner from 'qr-scanner'
 import styled from 'styled-components'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import closeIcon from '@/assets/icons/close.svg'
-
-const VideoArea = styled.div`
-  display: flex;
-  flex-direction: column;
-  inset: 0;
-  position: relative;
-  width: 100%;
-  height: 100dvh;
-  align-items: center;
-  justify-content: center;
-  background: #000;
-`
-
-const SquareBoxArea = styled.div`
-  position: absolute;
-  width: 14.9375rem;
-  height: 14.9375rem;
-  border-radius: 1.875rem;
-  pointer-events: none;
-  border: 8px solid #25af94;
-  box-shadow: 0 0 0 9999px rgba(6, 52, 40, 0.5);
-`
-
-const QrGuideText = styled.div`
-  position: absolute;
-  color: #fff;
-  text-align: center;
-  font-family: 'Pretendard Variable';
-  font-size: 1.25rem;
-  transform: translateY(2rem);
-  font-weight: 600;
-  top: 68%;
-  z-index: 2;
-  pointer-events: none;
-`
-
-const CloseBtn = styled.button`
-  background: transparent;
-  border: none;
-  position: absolute;
-  z-index: 3;
-  top: 3%;
-  right: 10%;
-  cursor: pointer;
-
-  img {
-    display: block;
-  }
-`
-
-const Video = styled.video`
-  position: absolute;
-  inset: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  background: #000;
-`
+import * as S from '@/components/QrScan/QrScan.Styled'
 
 export default React.memo(function Qrcode(props) {
   const navigate = useNavigate()
@@ -106,13 +49,6 @@ export default React.memo(function Qrcode(props) {
     stopCameraTracks()
   }
 
-  // const handleClose = () => {
-  //   scannerRef.current?.stop().catch(() => {})
-  //   scannerRef.current?.destroy()
-  //   videoRef.current?.srcObject?.getTracks?.().forEach((t) => t.stop())
-  //   navigate(-1)
-  // }
-
   const handleClose = () => {
     disposeScanner()
     navigate(-1)
@@ -147,17 +83,8 @@ export default React.memo(function Qrcode(props) {
       } catch {
         if (mounted) setQrError(true)
       }
-
-      // qrScanner.start().catch(() => mounted && setQrError(true))
     })
-    // return () => {
-    //   mounted = false
-    //   if (scannerRef.current) {
-    //     scannerRef.current.stop().catch(() => {})
-    //     scannerRef.current.destroy()
-    //     scannerRef.current = null
-    //   }
-    // }
+
     return () => {
       mounted = false
       disposeScanner() // 브라우저 '이전' 시에도 안전하게 정리
@@ -168,18 +95,18 @@ export default React.memo(function Qrcode(props) {
     <main id='qrcode' style={{ width: '100%', height: '100%' }}>
       {}
       {!qrError && (
-        <VideoArea>
-          <CloseBtn>
+        <S.VideoArea>
+          <S.CloseBtn>
             <img src={closeIcon} onClick={handleClose} />
-          </CloseBtn>
-          <Video ref={videoRef} playsInline muted autoPlay />
-          <SquareBoxArea />
-          <QrGuideText>
+          </S.CloseBtn>
+          <S.Video ref={videoRef} playsInline muted autoPlay />
+          <S.SquareBoxArea />
+          <S.QrGuideText>
             {' '}
             카페 키오스크에 부착돼있는
             <br /> QR코드를 스캔해주세요
-          </QrGuideText>
-        </VideoArea>
+          </S.QrGuideText>
+        </S.VideoArea>
       )}
       {}
       {qrError && (
