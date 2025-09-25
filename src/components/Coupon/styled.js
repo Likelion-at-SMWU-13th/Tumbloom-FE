d5c87b870634d1ab76e7144e691faa311a3f9f53
chootkuri <doyadoya@sookmyung.ac.kr>
2025-09-25 23:04:43 +0900

    쿠폰 스타일 컴포넌트 파일 분리

--- src/components/Coupon/styled.js
+++ src/components/Coupon/styled.js
@@ -302,3 +302,58 @@ export const AvailableCouponText = styled.p`
   line-height: normal;
   margin: 0;
 `
+
+// CouponPrev.jsx
+
+export const HeaderBox = styled.div`
+  display: grid;
+  grid-template-columns: 3.5rem 1fr 3.5rem;
+  width: 24.5625rem;
+  height: 3.5rem;
+  box-sizing: border-box;
+  position: fixed;
+  top: 0;
+  z-index: 10;
+  padding-top: 0;
+  flex-shrink: 0;
+  background-color: #fff;
+  border-bottom: 1px solid #d9d9d9;
+`
+
+export const LeftArea = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 0.5rem;
+  padding-left: 1.65rem;
+`
+
+export const BackBtn = styled.img`
+  width: 1.5rem;
+  height: 1.5rem;
+  flex-shrink: 0;
+  aspect-ratio: 1/1;
+  cursor: pointer;
+`
+
+export const TitleArea = styled.div`
+  margin: 0;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  color: #000;
+  text-align: center;
+  font-family: 'Pretendard Variable';
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  padding-top: 0.5rem;
+`
+
+export const RightArea = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  padding-top: 0.5rem;
+`
